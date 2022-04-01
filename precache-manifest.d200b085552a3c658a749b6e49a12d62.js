28567c9ef83217512e17a7099c31eb3d78fc62db
Alan Tram <atram@ucsd.edu>
2022-04-01 00:09:32 -0700

    Updates

---
+++ precache-manifest.d200b085552a3c658a749b6e49a12d62.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d382deac467a224460f4c135304cfbf3",
+    "url": "/atramUCSD/My-Website.git/index.html"
+  },
+  {
+    "revision": "2c2416c777f93ef08521",
+    "url": "/atramUCSD/My-Website.git/static/css/main.ba9f53cf.chunk.css"
+  },
+  {
+    "revision": "b77abf701024767c16f7",
+    "url": "/atramUCSD/My-Website.git/static/js/2.61a70d07.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/atramUCSD/My-Website.git/static/js/2.61a70d07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c2416c777f93ef08521",
+    "url": "/atramUCSD/My-Website.git/static/js/main.6338104a.chunk.js"
+  },
+  {
+    "revision": "a78f19754df6b5a8a101",
+    "url": "/atramUCSD/My-Website.git/static/js/runtime-main.1263e290.js"
+  },
+  {
+    "revision": "437937736b873f0b02739465c6ca5e16",
+    "url": "/atramUCSD/My-Website.git/static/media/about.43793773.gif"
+  },
+  {
+    "revision": "456d27d30afacf5ef9c2d04e25452c99",
+    "url": "/atramUCSD/My-Website.git/static/media/mock1.456d27d3.png"
+  },
+  {
+    "revision": "37e11540fe7357463bd40d3effb35da1",
+    "url": "/atramUCSD/My-Website.git/static/media/mock2.37e11540.png"
+  },
+  {
+    "revision": "6a736fcd570ebcf9699f8d5e9f949f60",
+    "url": "/atramUCSD/My-Website.git/static/media/mock3.6a736fcd.png"
+  },
+  {
+    "revision": "bd6a383d15853619853b9eba50524095",
+    "url": "/atramUCSD/My-Website.git/static/media/mock4.bd6a383d.png"
+  },
+  {
+    "revision": "4f2463af3e32cc31dd1dfaeae7857ef1",
+    "url": "/atramUCSD/My-Website.git/static/media/self.4f2463af.png"
+  }
+]);
